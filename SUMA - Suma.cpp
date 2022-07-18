735fd5b9766af6be5fd6ad9ef92ba85b96a76b4e
klein <klein@LAPTOP-KAGE>
2022-07-18 21:06:12 +0200

    Dodaj pliki projektów.

---
+++ SUMA - Suma.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int l1;
+
+void licz()
+{
+	int y = 0;
+	while (!cin.eof())
+	{
+		cin >> l1;
+		y = y + l1;
+		cout << y << endl;
+	}
+}
+
+int main()
+{
+	licz();
+}
